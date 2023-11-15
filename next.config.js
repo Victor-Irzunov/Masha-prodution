caf91a355a8775f5ca5a59193c4e298ec88b46e7
Виктор <viktor@MacBook-Air-Viktor.local>
2023-11-15 20:46:30 +0300

    --8

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	publicRuntimeConfig: {
-	  NEXT_PUBLIC_BASE_URL: 'https://vi-tech.site/',
-	},
 	images: {
 	  domains: ['vi-tech.site'],
 	},
